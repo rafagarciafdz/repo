210b5c237c23e63c51ea6e3e5ad4c7b5b8c067af
Rafael García <rafagarciafdz@gmail.com>
2015-05-09 12:38:03 -0500

    carita sorprendidad

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ asi podre evaluar el merge de manera adecuada
 empezemos
 
 AHora agrego esa linea para indicar que en esete moemnto estoy en el branch-1
-
+=O
 =)
 
 Ago manualmente el merge de "Esta es la otra linea que  que cumple la funcion de modificacion por separado para el branch master" con "Ahora esta linea es agregada desde el branch -1 para provocar que en ambos branches se hagan cambios por separado"
