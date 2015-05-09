4a742e7b07b88d397e106e7ec8fa8e4b138deb15
Rafael García <rafagarciafdz@gmail.com>
2015-05-09 12:10:15 -0500

    Agregada otra linea por separado para el branch master

--- main.c
+++ main.c
@@ -9,3 +9,5 @@ empezemos
 AHora agrego esa linea para indicar que en esete moemnto estoy en el branch-1
 
 =)
+
+Esta es la otra linea que  que cumple la funcion de modificacion por separado para el branch master
